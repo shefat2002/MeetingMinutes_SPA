f395741e62db23b1d73d4e526f1e0ad0fe865350
shefat2002 <shefat2002@gmail.com>
2025-07-06 18:40:58 +0600

    Fixed more UI Problem (#2)

--- MeetingMinutes.Web/obj/Debug/net8.0/MeetingMinutes.Web.AssemblyInfo.cs
+++ MeetingMinutes.Web/obj/Debug/net8.0/MeetingMinutes.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingMinutes.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f23ee3db06b0888b98a3beb1520290cea40360")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042b0d7e360637808f90fb31050bd9bdcdf61311")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingMinutes.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingMinutes.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
